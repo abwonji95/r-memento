6108d2a0a2d387fc0d341694fafe3ff07b506fad
abwonji95 <abwonji95@gmail.com>
2022-04-20 22:30:04 +0300

    services pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HolidayComponent } from './services/holiday/holiday.component';
+import { CountryComponent } from './services/country/country.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HolidayComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
