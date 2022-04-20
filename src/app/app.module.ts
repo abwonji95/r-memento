e098023dc8fdbc593f711f9911fab1ad7a2b9839
abwonji95 <abwonji95@gmail.com>
2022-04-20 22:51:36 +0300

    config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HolidayComponent } from './services/holiday/holiday.component';
 import { CountryComponent } from './services/country/country.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CountryComponent } from './services/country/country.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
