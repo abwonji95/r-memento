6eecbd4e8f42b092db422d6c3ea52bc3cd92ee1e
abwonji95 <abwonji95@gmail.com>
2022-08-31 09:06:00 +0300

    process

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,12 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { CountriesService } from 'src/app/services/countries.service';
 import { HolidayTodayService } from 'src/app/services/holiday-today.service';
 import { YearService } from 'src/app/services/year.service';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+/**
+ * @title Highlight the first autocomplete option
+ */
 
 @Component({
   selector: 'app-home',
@@ -15,15 +21,29 @@ export class HomeComponent implements OnInit {
   form=FormGroup;
   select_country=FormControl
   data_today=FormControl
+  
+  myControl = new FormControl('');
+  options: string[] = [];
+  filteredOptions: Observable<string[]> | undefined;
 
 
   constructor(private Countryservice:CountriesService,private yearService:YearService,private holidayToday:HolidayTodayService) { }
 
   ngOnInit(): void {
-    this.Countryservice.get().subscribe((country)=>{this.countries=country})
+    this.Countryservice.get().subscribe((country)=>{this.options=country})
     this.holidayToday.get().subscribe((holiday)=>{this.holiday=holiday.response.holidays})
-console.log(this.countries)
-console.log(this.holiday)
+    console.log("here ...")
+    console.log(this.options)
+
+this.filteredOptions = this.myControl.valueChanges.pipe(
+  startWith(''),
+  map(value => this._filter(value || '')),
+);
+  }
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
